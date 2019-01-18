1108396e4d00dfd41c32b698bb7937004b1de2d3
Arnaud Verrier <arnaudverrier@MBP-de-Arnaud.tagattitude.local>
2019-01-18 14:44:24 +0100

    Create project

---
+++ Escarmoosh/Escarmoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Escarmoosh
+//
+//  Created by Arnaud Verrier on 18/01/2019.
+//  Copyright © 2019 escarmoosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
